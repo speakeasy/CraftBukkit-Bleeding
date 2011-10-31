469089cdae2cffa3cd83eac25d062c97563666c1
Andrew Ardill <andrew.ardill@gmail.com>
2011-10-31 19:41:45 +1100

    Test-Server: Ensure the server runs a few ticks before shutting it down.

--- src/test/java/org/bukkit/craftbukkit/test/CraftBukkitTest.java
+++ src/test/java/org/bukkit/craftbukkit/test/CraftBukkitTest.java
@@ -4,16 +4,28 @@ import org.bukkit.craftbukkit.CraftServer;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
+import java.sql.Time;
+import java.util.Date;
+
 public class CraftBukkitTest {
     protected static CraftServer server;
+    private static long startup;
 
     @BeforeClass
     public static void oneTimeSetUp() throws InterruptedException {
         server = TestUtils.startServer();
+        startup = new Date().getTime();
     }
 
     @AfterClass
     public static void oneTimeTearDown() {
+        try {
+            while(new Date().getTime() - startup < 5000) {
+                // wait at least a couple of ticks before shutting down
+                Thread.sleep(5000);
+            }
+        } catch (InterruptedException ignored) {
+        }
         TestUtils.shutdownServer(server);
     }
 }
