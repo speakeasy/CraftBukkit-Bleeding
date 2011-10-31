c5367c89a37ed4acd9f0161e4a479f3f9dcd2d9e
Andrew Ardill <andrew.ardill@gmail.com>
2011-10-31 19:40:59 +1100

    Test-Server: Always use the same seed

--- src/test/java/org/bukkit/craftbukkit/test/TestUtils.java
+++ src/test/java/org/bukkit/craftbukkit/test/TestUtils.java
@@ -2,13 +2,14 @@ package org.bukkit.craftbukkit.test;
 
 import joptsimple.OptionSet;
 import net.minecraft.server.MinecraftServer;
+import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.craftbukkit.Main;
 
 public class TestUtils {
     private static CraftServer server;
     private static OptionSet defaultOptions;
-    private static String[] defaultArgs = {"-log-append", "false"};
+    private static String[] defaultArgs = {"level-seed", "31337"};
 
     public static CraftServer startServer() throws InterruptedException {
         if (server == null) {
