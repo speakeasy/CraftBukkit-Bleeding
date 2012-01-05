3b24ccb5fdef467e51e562e6ca909ee2fe94298a
Erik Broes <erikbroes@grum.nl>
2012-01-05 17:32:36 +0100

    Clean

--- src/main/java/net/minecraft/server/BlockCauldron.java
+++ src/main/java/net/minecraft/server/BlockCauldron.java
@@ -73,9 +73,9 @@ public class BlockCauldron extends Block {
 
                         if (!entityhuman.inventory.pickup(itemstack1)) {
                             world.addEntity(new EntityItem(world, (double) i + 0.5D, (double) j + 1.5D, (double) k + 0.5D, itemstack1));
-                        } else { //Craftbukkit start
-                            ((EntityPlayer)entityhuman).updateInventory(((EntityPlayer)entityhuman).defaultContainer);
-                        }// Craftbukkit end
+                        } else if (entityhuman instanceof EntityPlayer) { //CraftBukkit
+                            ((EntityPlayer) entityhuman).updateInventory(entityhuman.defaultContainer); // CraftBukkit
+                        }
 
                         --itemstack.count;
                         if (itemstack.count <= 0) {
