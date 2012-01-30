21c5ac2e7cecd724f940554a3d312aeb65bde1a7
Erik Broes <erikbroes@grum.nl>
2012-01-30 21:51:53 +0100

    Fix + Clean from deprecation clean.

---
+++ src/test/java/org/bukkit/StatisticTest.java
@@ -0,0 +1,43 @@
+package org.bukkit;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import net.minecraft.server.Statistic;
+import net.minecraft.server.StatisticList;
+
+import org.bukkit.support.Util;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+public class StatisticTest {
+    @Test
+    @SuppressWarnings("unchecked")
+    public void verifyMapping() throws IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException {
+        List<org.bukkit.Statistic> statistics = Lists.newArrayList(org.bukkit.Statistic.values());
+
+        for (Statistic statistic : (List<Statistic>) StatisticList.b) {
+            int id = statistic.e;
+            String hash = statistic.g;
+
+            System.err.println(String.format("%d %s %s", id, Integer.toHexString(id).toUpperCase(), statistic));
+
+//            if ((id & Achievement.STATISTIC_OFFSET) != Achievement.STATISTIC_OFFSET) continue;
+//            if (hash == null) continue;
+//
+//            String name = Util.getInternalState(Statistic.class, statistic, "a");
+//            String message = String.format("org.bukkit.Achievement is missing id: %d named: '%s'", id - Achievement.STATISTIC_OFFSET, name);
+//
+//            Achievement subject = Achievement.getById(id);
+//            assertNotNull(message, subject);
+//
+//            achievements.remove(subject);
+        }
+
+//        assertThat("org.bukkit.Achievement has too many achievements", achievements, hasSize(0));
+    }
+}
