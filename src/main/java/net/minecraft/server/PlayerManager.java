32ae38d3736d029a75ac76d9fece687116d23e73
Mike Primm <mike@primmhome.com>
2012-03-08 00:58:30 -0600

    Fix for BUKKIT-1060 : only do unload-all on tick when last player leaves

--- src/main/java/net/minecraft/server/PlayerManager.java
+++ src/main/java/net/minecraft/server/PlayerManager.java
@@ -12,6 +12,7 @@ public class PlayerManager {
     private int e;
     private int f;
     private final int[][] g = new int[][] { { 1, 0}, { 0, 1}, { -1, 0}, { 0, -1}};
+    private boolean wasNotEmpty; // CraftBukkit
 
     public PlayerManager(MinecraftServer minecraftserver, int i, int j) {
         if (j > 15) {
@@ -39,10 +40,16 @@ public class PlayerManager {
             WorldServer worldserver = this.server.getWorldServer(this.e);
             WorldProvider worldprovider = worldserver.worldProvider;
 
-            if (!worldprovider.c()) {
+            if (!worldprovider.c() && wasNotEmpty) { // CraftBukkit - only do unload when we go from non-empty to empty
                 worldserver.chunkProviderServer.c();
             }
+            wasNotEmpty = false; // CraftBukkit
         }
+        // CraftBukkit start
+        else {  
+            wasNotEmpty = true;
+        }
+        // CraftBukkit end
     }
 
     private PlayerInstance a(int i, int j, boolean flag) {
