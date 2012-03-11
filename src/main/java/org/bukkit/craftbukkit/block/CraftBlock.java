ed11464c4a02289cd6d6b2a4bed953ed0975a6e4
Celtic Minstrel <celtic.minstrel.ca@some.place>
2012-03-11 19:29:35 -0400

    Fix BlockFace directions

--- src/main/java/org/bukkit/craftbukkit/block/CraftBlock.java
+++ src/main/java/org/bukkit/craftbukkit/block/CraftBlock.java
@@ -186,13 +186,13 @@ public class CraftBlock implements Block {
         case 1:
             return BlockFace.UP;
         case 2:
-            return BlockFace.EAST;
-        case 3:
-            return BlockFace.WEST;
-        case 4:
             return BlockFace.NORTH;
-        case 5:
+        case 3:
             return BlockFace.SOUTH;
+        case 4:
+            return BlockFace.WEST;
+        case 5:
+            return BlockFace.EAST;
         default:
             return BlockFace.SELF;
         }
