d2ea3be6718fe30f273b4558cb1a85e2787aa522
Wesley Wolfe <weswolf@aol.com>
2012-12-01 17:09:44 -0600

    Merge ItemStack base tests

--- src/test/java/org/bukkit/craftbukkit/inventory/ItemStackLoreEnchantmentTest.java
+++ src/test/java/org/bukkit/craftbukkit/inventory/ItemStackLoreEnchantmentTest.java
@@ -1,7 +1,9 @@
 package org.bukkit.craftbukkit.inventory;
 
+import java.util.Arrays;
 import java.util.List;
 
+import org.bukkit.inventory.ItemStack;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -15,6 +17,89 @@ public class ItemStackLoreEnchantmentTest extends ItemStackTests {
     }
 
     static List<Object[]> operaters() {
-        return CompoundOperater.compound("%s+%s", NAME_PARAMETER, 0l, ItemStackLoreTest.operaters(), ItemStackEnchantmentTest.operaters());
+        return CompoundOperater.compound(
+            "%s+%s",
+            NAME_PARAMETER,
+            ~0l,
+            Arrays.asList(
+                new Object[] {
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            // TODO Add Lore
+                            return cleanStack;
+                        }
+                    },
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            // TODO Add other lore
+                            return cleanStack;
+                        }
+                    },
+                    "Lore1"
+                },
+                new Object[] {
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            // TODO Add name
+                            return cleanStack;
+                        }
+                    },
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            // TODO Add other name
+                            return cleanStack;
+                        }
+                    },
+                    "Name1"
+                },
+                new Object[] {
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            // TODO Add name / lore
+                            return cleanStack;
+                        }
+                    },
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            // TODO Add other name / lore
+                            return cleanStack;
+                        }
+                    },
+                    "LoreAndName1"
+                }
+            ),
+            Arrays.asList(
+                new Object[] {
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            // TODO Add enchantment
+                            return cleanStack;
+                        }
+                    },
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            // TODO Add other enchantment
+                            return cleanStack;
+                        }
+                    },
+                    "Enchant1"
+                },
+                new Object[] {
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            // TODO Add enchantment
+                            return cleanStack;
+                        }
+                    },
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            // TODO Add other enchantment
+                            return cleanStack;
+                        }
+                    },
+                    "Enchant2"
+                }
+            )
+        );
     }
 }
