a10fb1badb13baad6c90d5a5dedfe3bafa927f63
Wesley Wolfe <weswolf@aol.com>
2012-12-01 20:21:51 -0600

    Finish up Skull tests

--- src/test/java/org/bukkit/craftbukkit/inventory/SkullItemStackTest.java
+++ src/test/java/org/bukkit/craftbukkit/inventory/SkullItemStackTest.java
@@ -10,54 +10,78 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import com.google.common.base.Joiner;
+
 @RunWith(Parameterized.class)
 public class SkullItemStackTest extends ItemStackTests {
 
     @Parameters(name="[{index}]:{" + NAME_PARAMETER + "}")
     public static List<Object[]> data() {
-        return StackProvider.compound(operaters(), "%s %s", NAME_PARAMETER, Material.SKULL, Material.SKULL_ITEM);
+        return StackProvider.compound(operaters(), "%s %s", NAME_PARAMETER, Material.SKULL_ITEM);
     }
 
     static List<Object[]> operaters() {
-        return Arrays.asList(
-            new Object[] {
-                new Operater() {
-                    public ItemStack operate(ItemStack cleanStack) {
-                        SkullMeta meta = (SkullMeta) cleanStack.getItemMeta();
-                        meta.setOwner("Notch");
-                        cleanStack.setItemMeta(meta);
-                        return cleanStack;
-                    }
+        return CompoundOperater.compound(
+            Joiner.on('+'),
+            NAME_PARAMETER,
+            Long.parseLong("10", 2),
+            ItemStackLoreEnchantmentTest.operaters(),
+            Arrays.asList(
+                new Object[] {
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            SkullMeta meta = (SkullMeta) cleanStack.getItemMeta();
+                            meta.setOwner("Notch");
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            SkullMeta meta = (SkullMeta) cleanStack.getItemMeta();
+                            meta.setOwner("Dinnerbone");
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    "Name 1 vs. Name 2"
                 },
-                new Operater() {
-                    public ItemStack operate(ItemStack cleanStack) {
-                        SkullMeta meta = (SkullMeta) cleanStack.getItemMeta();
-                        meta.setOwner("PlantAssassin");
-                        cleanStack.setItemMeta(meta);
-                        return cleanStack;
-                    }
+                new Object[] {
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            SkullMeta meta = (SkullMeta) cleanStack.getItemMeta();
+                            meta.setOwner("Notch");
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            SkullMeta meta = (SkullMeta) cleanStack.getItemMeta();
+                            meta.setOwner(null);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    "Name vs. Null"
                 },
-                "Name 1 vs. Name 2"
-            },
-            new Object[] {
-                new Operater() {
-                    public ItemStack operate(ItemStack cleanStack) {
-                        SkullMeta meta = (SkullMeta) cleanStack.getItemMeta();
-                        meta.setOwner("Notch");
-                        cleanStack.setItemMeta(meta);
-                        return cleanStack;
-                    }
-                },
-                new Operater() {
-                    public ItemStack operate(ItemStack cleanStack) {
-                        SkullMeta meta = (SkullMeta) cleanStack.getItemMeta();
-                        meta.setOwner(null);
-                        cleanStack.setItemMeta(meta);
-                        return cleanStack;
-                    }
-                },
-                "Name vs. Null"
-            }
+                new Object[] {
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            SkullMeta meta = (SkullMeta) cleanStack.getItemMeta();
+                            meta.setOwner("Notch");
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    new Operater() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            return cleanStack;
+                        }
+                    },
+                    "Name vs. None"
+                }
+            )
         );
     }
 }
