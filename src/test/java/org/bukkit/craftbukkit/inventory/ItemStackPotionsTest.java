d312b0253c380a0b4824662a0696dfbfd9b06f68
Wesley Wolfe <weswolf@aol.com>
2012-12-16 01:44:16 -0600

    Add potion tests
    I loled...
    -- Out work is never over

--- src/test/java/org/bukkit/craftbukkit/inventory/ItemStackPotionsTest.java
+++ src/test/java/org/bukkit/craftbukkit/inventory/ItemStackPotionsTest.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffectType;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+
+import com.google.common.base.Joiner;
 
 @RunWith(Parameterized.class)
 public class ItemStackPotionsTest extends ItemStackTest {
@@ -18,37 +22,124 @@ public class ItemStackPotionsTest extends ItemStackTest {
     }
 
     static List<Object[]> operators() {
-        return Arrays.asList(
-            new Object[] {
-                new Operator() {
-                    public ItemStack operate(ItemStack cleanStack) {
-                        // TODO do POTION stuff
-                        return cleanStack;
-                    }
+        return CompoundOperator.compound(
+            Joiner.on('+'),
+            NAME_PARAMETER,
+            Long.parseLong("10", 2),
+            ItemStackLoreEnchantmentTest.operators(),
+            Arrays.asList(
+                new Object[] {
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            meta.addCustomEffect(PotionEffectType.CONFUSION.createEffect(1, 1), false);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            return cleanStack;
+                        }
+                    },
+                    "Potion vs Null"
                 },
-                new Operator() {
-                    public ItemStack operate(ItemStack cleanStack) {
-                        // TODO Do other POTION stuff
-                        return cleanStack;
-                    }
+                new Object[] {
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            meta.addCustomEffect(PotionEffectType.HARM.createEffect(2, 1), false);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    "Potion vs Blank"
                 },
-                "NoChange"
-            },
-            new Object[] {
-                new Operator() {
-                    public ItemStack operate(ItemStack cleanStack) {
-                        // TODO Do POTION stuff
-                        return cleanStack;
-                    }
+                new Object[] {
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            meta.addCustomEffect(PotionEffectType.SLOW_DIGGING.createEffect(1, 1), false);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            meta.addCustomEffect(PotionEffectType.FAST_DIGGING.createEffect(1, 1), false);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    "Potion vs Harder"
                 },
-                new Operator() {
-                    public ItemStack operate(ItemStack cleanStack) {
-                        // TODO Do other POTION stuff
-                        return cleanStack;
-                    }
+                new Object[] {
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            meta.addCustomEffect(PotionEffectType.JUMP.createEffect(1, 1), false);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            meta.addCustomEffect(PotionEffectType.JUMP.createEffect(1, 1), false);
+                            meta.addCustomEffect(PotionEffectType.REGENERATION.createEffect(1, 1), false);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    "Potion vs Better"
                 },
-                "POTION2"
-            }
+                new Object[] {
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            meta.addCustomEffect(PotionEffectType.SPEED.createEffect(10, 1), false);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            meta.addCustomEffect(PotionEffectType.SPEED.createEffect(5, 1), false);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    "Potion vs Faster"
+                },
+                new Object[] {
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            meta.addCustomEffect(PotionEffectType.INCREASE_DAMAGE.createEffect(1, 1), false);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    new Operator() {
+                        public ItemStack operate(ItemStack cleanStack) {
+                            final PotionMeta meta = (PotionMeta) cleanStack.getItemMeta();
+                            meta.addCustomEffect(PotionEffectType.INCREASE_DAMAGE.createEffect(1, 2), false);
+                            cleanStack.setItemMeta(meta);
+                            return cleanStack;
+                        }
+                    },
+                    "Potion vs Stronger"
+                }
+            )
         );
     }
 }
