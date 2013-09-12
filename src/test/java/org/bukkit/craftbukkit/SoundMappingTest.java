2970f927eb6b79d0e71a813984097de7aef86609
feildmaster <admin@feildmaster.com>
2013-09-11 23:42:19 -0500

    Break everything trying to "paramatize" the sound test.
    It seemed like a good idea... However I can't work my mind around it.

--- src/test/java/org/bukkit/craftbukkit/SoundMappingTest.java
+++ src/test/java/org/bukkit/craftbukkit/SoundMappingTest.java
@@ -14,6 +14,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.bukkit.Sound;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runners.Parameterized.*;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
@@ -21,15 +22,15 @@ import org.xml.sax.SAXException;
  * This file opens a connection to check for sound availability.
  */
 public final class SoundMappingTest {
-    private static final List<String> EXPECTED_ARRAY = new ArrayList<String>();
-    private static final Pattern SOUND_PATTERN = Pattern.compile("sound/([a-zA-Z/_ ]+)[0-9]*\\.ogg");
-    private static final List<String> LIVE_SOUNDS = new ArrayList<String>();
-    private static final List<String> EXISTING_SOUNDS = new ArrayList<String>();
+    private static final Set<String> MINECRAFT_SOUNDS = new HashSet<String>();
+    private static final Map<String, Sound> BUKKIT_SOUNDS = new HashMap<String, Sound>();
 
     @BeforeClass
     public static void processSounds() throws MalformedURLException, ParserConfigurationException, SAXException, IOException {
         final URL url = new URL("https://s3.amazonaws.com/Minecraft.Resources/");
         final NodeList nodeList = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url.openConnection().getInputStream()).getElementsByTagName("Contents");
+
+        final Pattern pattern = Pattern.compile("sound/([a-zA-Z/_ ]+)[0-9]*\\.ogg");
 
         for (int i = 0; i < nodeList.getLength(); i++) {
             final Node node = nodeList.item(i);
@@ -37,34 +38,43 @@ public final class SoundMappingTest {
             if (node.getNodeType() == 1) {
                 final Element element = (Element) node;
                 final String key = element.getElementsByTagName("Key").item(0).getChildNodes().item(0).getNodeValue();
-                final Matcher matcher = SOUND_PATTERN.matcher(key);
+                final Matcher matcher = pattern.matcher(key);
 
                 if (matcher.matches()) {
                     final String sound = matcher.group(1).replace('/', '.');
-                    if (!LIVE_SOUNDS.contains(sound)) {
-                        LIVE_SOUNDS.add(sound);
-                    }
+                    MINECRAFT_SOUNDS.add(sound);
                 }
             }
         }
 
         for (Sound sound : Sound.values()) {
-            EXISTING_SOUNDS.add(CraftSound.getSound(sound));
+            BUKKIT_SOUNDS.put(CraftSound.getSound(sound), sound);
+        }
+    }
+
+    @Parameters(name="Sound[{index}]: {0}<{1}>")
+    public static List<Object[]> data() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        for (Sound sound : Sound.values()) {
+            list.add(new Object[] {sound, CraftSound.getSound(sound)});
+        }
+        return list;
+    }
+
+    @Parameter(0) public Sound bukitsound;
+    @Parameter(1) public String craftsound;
+
+    @Test
+    public void existsInMinecraft() {
+        if (!craftsound.isEmpty()) {
+            assertThat(MINECRAFT_SOUNDS, contains(craftsound));
         }
     }
 
     @Test
-    public void allSoundsExistInMinecraft() {
-        // Assert all current sounds are live
-        final List<String> removed_sounds_from_minecraft = new ArrayList<String>(EXISTING_SOUNDS);
-        removed_sounds_from_minecraft.removeAll(LIVE_SOUNDS);
-        assertEquals("Expected all bukkit sounds to exist!", EXPECTED_ARRAY, removed_sounds_from_minecraft);
-    }
-
-    @Test
-    public void allSoundsExistInBukkit() {
+    public void existsInBukkit() {
         // Assert there are no new sounds
-        final List<String> missing_sounds_from_bukkit = new ArrayList<String>(LIVE_SOUNDS);
+        final List<String> missing_sounds_from_bukkit = new ArrayList<String>(MINECRAFT_SOUNDS);
         missing_sounds_from_bukkit.removeAll(EXISTING_SOUNDS);
         assertEquals("Expected no unmapped sounds in bukkit!", EXPECTED_ARRAY, missing_sounds_from_bukkit);
     }
