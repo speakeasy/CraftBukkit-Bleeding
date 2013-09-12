d952fa61af6866e436bdc2c37b751dabeda2ed9d
feildmaster <admin@feildmaster.com>
2013-09-12 01:35:56 -0500

    Parameterized sound tests. :D

--- src/test/java/org/bukkit/SoundTest.java
+++ src/test/java/org/bukkit/SoundTest.java
@@ -3,16 +3,32 @@ package org.bukkit;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.craftbukkit.CraftSound;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
-
+@RunWith(Parameterized.class)
 public class SoundTest {
+    @Parameters(name="Sound[{index}]: {0}")
+    public static List<Object[]> data() {
+        final List<Object[]> list = new ArrayList<Object[]>();
+
+        for (Sound sound : Sound.values()) {
+            list.add(new Object[] {sound});
+        }
+
+        return list;
+    }
+
+    @Parameter(0) public Sound sound;
 
     @Test
-    public void testGetSound() {
-        for (Sound sound : Sound.values()) {
-            assertThat(sound.name(), CraftSound.getSound(sound), is(not(nullValue())));
-        }
+    public void hasCraftSound() {
+        assertThat(CraftSound.getSound(sound), is(not(nullValue())));
     }
 }
--- src/test/java/org/bukkit/craftbukkit/SoundMappingTest.java
+++ src/test/java/org/bukkit/craftbukkit/SoundMappingTest.java
@@ -1,8 +1,10 @@
 package org.bukkit.craftbukkit;
 
-import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,25 +14,29 @@ import java.util.regex.Pattern;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.bukkit.Sound;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runners.Parameterized.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 /**
  * This file opens a connection to check for sound availability.
  */
+@RunWith(Parameterized.class)
 public final class SoundMappingTest {
-    private static final Set<String> MINECRAFT_SOUNDS = new HashSet<String>();
-    private static final Map<String, Sound> BUKKIT_SOUNDS = new HashMap<String, Sound>();
+    private static Set<String> MINECRAFT_SOUNDS;
+    private static Map<String, Sound> BUKKIT_SOUNDS;
 
-    @BeforeClass
     public static void processSounds() throws MalformedURLException, ParserConfigurationException, SAXException, IOException {
         final URL url = new URL("https://s3.amazonaws.com/Minecraft.Resources/");
         final NodeList nodeList = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url.openConnection().getInputStream()).getElementsByTagName("Contents");
 
         final Pattern pattern = Pattern.compile("sound/([a-zA-Z/_ ]+)[0-9]*\\.ogg");
+
+        final ImmutableSet.Builder<String> set_builder = ImmutableSet.<String>builder();
 
         for (int i = 0; i < nodeList.getLength(); i++) {
             final Node node = nodeList.item(i);
@@ -42,40 +48,60 @@ public final class SoundMappingTest {
 
                 if (matcher.matches()) {
                     final String sound = matcher.group(1).replace('/', '.');
-                    MINECRAFT_SOUNDS.add(sound);
+                    set_builder.add(sound);
                 }
             }
         }
 
+        final ImmutableMap.Builder<String, Sound> map_builder = ImmutableMap.<String, Sound>builder();
+
         for (Sound sound : Sound.values()) {
-            BUKKIT_SOUNDS.put(CraftSound.getSound(sound), sound);
+            final String craftsound = CraftSound.getSound(sound);
+            if (craftsound != null) {
+                map_builder.put(craftsound, sound);
+            }
         }
+
+        MINECRAFT_SOUNDS = set_builder.build();
+        BUKKIT_SOUNDS = map_builder.build();
     }
 
-    @Parameters(name="Sound[{index}]: {0}<{1}>")
-    public static List<Object[]> data() {
-        List<Object[]> list = new ArrayList<Object[]>();
-        for (Sound sound : Sound.values()) {
-            list.add(new Object[] {sound, CraftSound.getSound(sound)});
+    @Parameters(name="Sound[{index}]: {0}")
+    public static List<Object[]> data() throws MalformedURLException, MalformedURLException, ParserConfigurationException, SAXException, IOException {
+        processSounds();
+
+        final Set<String> data = new HashSet<String>();
+        data.addAll(MINECRAFT_SOUNDS);
+        data.addAll(BUKKIT_SOUNDS.keySet());
+
+        final List<Object[]> list = new ArrayList<Object[]>(data.size());
+
+        for (String sound : data) {
+            list.add(new Object[] {sound});
         }
+
+        Collections.sort(list, new Comparator<Object[]>() {
+            @Override
+            public int compare(Object[] o1, Object[] o2) {
+                return ((String) o1[0]).compareTo((String) o2[0]);
+            }
+        });
+
         return list;
     }
 
-    @Parameter(0) public Sound bukitsound;
-    @Parameter(1) public String craftsound;
-
-    @Test
-    public void existsInMinecraft() {
-        if (!craftsound.isEmpty()) {
-            assertThat(MINECRAFT_SOUNDS, contains(craftsound));
-        }
-    }
+    @Parameter(0) public String soundpath;
 
     @Test
     public void existsInBukkit() {
-        // Assert there are no new sounds
-        final List<String> missing_sounds_from_bukkit = new ArrayList<String>(MINECRAFT_SOUNDS);
-        missing_sounds_from_bukkit.removeAll(EXISTING_SOUNDS);
-        assertEquals("Expected no unmapped sounds in bukkit!", EXPECTED_ARRAY, missing_sounds_from_bukkit);
+        Sound bukkitsound = BUKKIT_SOUNDS.get(soundpath);
+        assertThat("Sound is not in bukkit", bukkitsound, is(not(nullValue())));
+    }
+
+    @Test
+    public void existsInMinecraft() {
+        if (!soundpath.isEmpty()) {
+            assertThat("Sound is no longer live", soundpath, isIn(MINECRAFT_SOUNDS));
+        }
     }
 }
